692c23cd61055896f9e0fdad852c43d77e0d23d2
jakaka12 <jakaka12@users.noreply.github.com>
2015-05-10 00:35:02 +1200

    toString added

--- Monster.java
+++ Monster.java
@@ -1,34 +1,42 @@
 /**
  * COSC343 Assignment 2
  * Monster.java
- * Purpose: 
+ * Purpose: Create monster objects with a location
  * 
  * @author: Jazlyn Akaka
- * @version: 29/4/15
+ * @version: 9/5/15
  */
 
 import java.util.*;
 
 public class Monster{
-	private int energy_level;
-	private Location loc; 
-	/**
-   	 * CONSTRUCTOR: Creates a Monster
-   	 */
-	public Monster(int row, int col){
-		loc = new Location(row,col);
-		
-	}
+    private Location loc; 
+    /**
+     * CONSTRUCTOR: Creates a Monster
+     */
+    public Monster(int row, int col){
+	loc = new Location(row,col);
+  
+    }
 
-	public Location getLoc(){
-		return loc;
-	}
+    public Location getLoc(){
+	return loc;
+    }
 
-	public static void main (String[] args){
-		Monster frank = new Monster(10,2,2);
-		System.out.println(frank.loc);
+    public void setLoc(Location l){
+	loc = l;
+    }
 
-	}
+    public String toString(){
+	String result = "Monster " + loc;
+	return result;
+    }
+    
+    public static void main (String[] args){
+	Monster frank = new Monster(10,2);
+	System.out.println(frank.loc);
+
+    }
 
 
 }
