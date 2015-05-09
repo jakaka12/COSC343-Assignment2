eae1288692e7d76776431bc0e13e31a79eb196da
jakaka12 <jakaka12@users.noreply.github.com>
2015-05-10 00:34:25 +1200

    toString added

--- Creature.java
+++ Creature.java
@@ -1,57 +1,57 @@
 /**
  * COSC343 Assignment 2
  * Creature.java
- * Purpose: 
+ * Purpose: Create creature objects with an energy level and a location
  * 
  * @author: Jazlyn Akaka
- * @version: 29/4/15
+ * @version: 9/5/15
  */
 
 import java.util.*;
 
 public class Creature{
-	private int energy_level;
-	private Location loc; 
-	/**
-   	 * CONSTRUCTOR: Creates a Creature 
-   	 */
-	public Creature(int energy, int row, int col){
-		energy_level = energy;
-		loc = new Location(row,col);
+    private int energy_level;
+    private Location loc; 
+    /**
+     * CONSTRUCTOR: Creates a Creature 
+     */
+    public Creature(int energy, int x, int y){
+	energy_level = energy;
+	loc = new Location(x,y);
 		
-	}
+    }
 
-	public int getEnergyLevel(){
-		return energy_level;
-	}
+    public int getEnergyLevel(){
+	return energy_level;
+    }
 
-	public Location getLoc(){
-		return loc;
-	}
+    public Location getLoc(){
+	return loc;
+    }
 
-	public boolean isDead(){
-		return (energy_level>0);
-	}
+    public void setLoc(Location l){
+	loc = l;
+    }
 
-	public void loseEnergy(int n){
-		energy_level-=n;
-	}
+    public boolean isAlive(){
+	return (energy_level>0);
+    }
 
-	public void moveTo(){
-		
-	}
+    public void loseEnergy(int n){
+	energy_level-=n;
+    }
 
-	public void moveAway(){
-		
-	}
+    public String toString(){
+	String result = "energy_level:" + energy_level;
+	result += "\t" + loc + "\t" + "alive:" + isAlive();
+	return result;
+    }
+    
+    public static void main (String[] args){
+	Creature gru = new Creature(10,2,2);
+	System.out.println(gru.loc);
 
-
-
-	public static void main (String[] args){
-		Creature gru = new Creature(10,2,2);
-		System.out.println(gru.loc);
-
-	}
+    }
 
 
 }
