29a2dd6aefb54dab8e0ec6460a374a92e6f0ed09
jakaka12 <jakaka12@users.noreply.github.com>
2015-05-11 01:10:17 +1200

    Create Chromosome.java

---
+++ Chromosome.java
@@ -0,0 +1,141 @@
+/** 
+ * COSC343 Assignment2
+ * Chromosome.java
+ * Purpose: Representation of a creature's chromosome, which determines
+ * the creature's actions. For the variables that determine direction to move,
+ * the directions are represented by integers. 
+ *          0:ignore
+ *          1:towards
+ *          2:away 
+ *          3:random
+ * For the default action, 
+ *          0:random
+ *          1:north
+ *          2:south
+ *          3:east
+ *          4:west         
+ *
+ * @author: Jazlyn Akaka
+ * @version: 10/5/15
+ */
+
+import java.util.*;
+
+public class Chromosome{
+    private boolean[] eatStrawb = {true,false};
+    private boolean[] eatMush = {true,false};
+    private int[] strawbMove = {0,1,2,3}; //direction to move given strawberry
+    private int[] mushMove = {0,1,2,3}; //direction to move given mushroom
+    private int[] monsMove = {0,1,2,3}; //direction to move given monster
+    private int[] creMove = {0,1,2,3}; //direction to move given a creature
+    private int[] action = {0,1,2,3,4}; //default action (random, north, south, east, west)
+    private Object[] chrome = new Object[13];
+    //protected int[] weights; //weights associated with the above roles
+    /** 
+     * CONSTRUCTOR: Creates an all encompasing chromosome that provides all possible values for roles
+     */
+    public Chromosome(){
+	Random rand = new Random();
+	chrome[0] = eatStrawb[rand.nextInt(eatStrawb.length)];
+	chrome[1] = eatMush[rand.nextInt(eatMush.length)];
+	chrome[2] = strawbMove[rand.nextInt(strawbMove.length)];
+	chrome[3] = mushMove[rand.nextInt(mushMove.length)];
+	chrome[4] = monsMove[rand.nextInt(monsMove.length)];
+	chrome[5] = creMove[rand.nextInt(creMove.length)];
+	chrome[6] = action[rand.nextInt(action.length)];
+	chrome[7] = rand.nextDouble();
+	chrome[8] = rand.nextDouble();
+	chrome[9] = rand.nextDouble();
+	chrome[10] = rand.nextDouble();
+	chrome[11] = rand.nextDouble();
+	chrome[12] = rand.nextDouble();
+    }
+
+    /************** GETTER METHODS *****************/
+    
+    /** Method gets value of eatStrawb
+     *  @return boolean value of eatStrawb
+     */
+    public boolean getEatStrawb(){
+	return (Boolean)chrome[0];
+    }
+
+    /** Method gets value of eatMush
+     * @ return boolean value of eatMuch
+     */
+    public boolean getEatMush(){
+	return (Boolean)chrome[1];
+    }
+
+    /** Method gets strawbMove
+     *  @return integer representation of towards, away, random, ignore
+     */
+    public int getStrawbMove(){
+	return (Integer)chrome[2];
+    }
+
+    /** Method gets mushMove
+     *  @return int representaiton of towards, away, random, ignore
+     */
+    public int getMushMove(){
+	return (Integer)chrome[3];
+    }
+
+    /** Method gets monsMove
+     *  @return int representaiton of towards, away, random, ignore
+     */
+    public int getMonsMove(){
+	return (Integer)chrome[4];
+    }
+
+    /** Method gets creMove
+     *  @return int representaiton of towards, away, random, ignore
+     */
+    public int getCreMove(){
+	return (Integer)chrome[5];
+    }
+
+    /** Method gets default action
+     *  @return int representaiton of random, north, south, east, west
+     */
+    public int getAction(){
+	return (Integer)chrome[6];
+    }
+
+    /** Method gets all the weights
+     *  @return an int[] of all of the weights that correspond to the roles
+     */
+    public double[] getWeights(){
+	double[] result = new double[6];
+	for (int i = 0; i<result.length; i++){
+	    result[i] = (Double)chrome[7+i];
+	}
+	return result;
+    }
+    
+    public String toString(){
+	String s = "[";
+	for (int i = 0; i<chrome.length-1; i++){
+	    s+= chrome[i] + ", ";
+	}
+	s += chrome[12]+ "]";
+	return s;
+    }
+
+    public static void main (String[] args){
+	Chromosome c = new Chromosome();
+	System.out.println(c);
+	System.out.println(c.getEatStrawb());
+	System.out.println(c.getEatMush());
+	System.out.println(c.getStrawbMove());
+	System.out.println(c.getMushMove());
+	System.out.println(c.getMonsMove());
+	System.out.println(c.getCreMove());
+	System.out.println(c.getAction());
+	double[] weights = c.getWeights();
+	for (int i = 0; i<weights.length; i++){
+	    System.out.println(weights[i]);
+	}
+    }
+
+}
